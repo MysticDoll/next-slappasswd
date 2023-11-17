2d5e896ceca499d85f8b1860adce905ae01ece7b
Mitsuru Takigahira <mysticdoll@mysticdoll.com>
2023-11-17 09:12:24 +0900

    implement slappasswd(8)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,85 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import React, { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+async function generateHash(rawPasswd: string, salt: Uint8Array): Promise<string> {
+    const encoder = new TextEncoder
+    const passwdArray = encoder.encode(rawPasswd)
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const passwdMerged = new Uint8Array(passwdArray.length + salt.length)
+    passwdMerged.set(passwdArray)
+    passwdMerged.set(salt, passwdArray.length)
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    const digest = await crypto.subtle.digest('SHA-1', passwdMerged)
+    const digestUint8Array = new Uint8Array(digest)
+    const hashedArray = new Uint8Array(digestUint8Array.length + salt.length)
+    hashedArray.set(digestUint8Array)
+    hashedArray.set(salt, digestUint8Array.length)
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const digestString = String.fromCharCode(...hashedArray)
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return `\${SSHA}${btoa(digestString)}`
 }
+
+export default function Home() {
+    const [rawPasswd, setRawPasswd] = useState('')
+    const [passwdHash, setPasswdHash] = useState('')
+    const [modalVisible, setModalVisible] = useState(false)
+    const [modalX, setModalX] = useState(0)
+    const [modalY, setModalY] = useState(0)
+
+    return (
+        <main
+            className={`flex min-h-screen flex-col items-center p-24 m${inter.className}`}
+        >
+            <div className="lg:flex lg:items-center lg:justify-between">
+                <div className="min-w-0 flex-1">
+                    <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">WEB slappasswd(8) Tool</h2>
+                </div>
+            </div>
+            <div className="mt-8 z-10 py-2 w-200 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex shadow">
+                <span>Type Raw Password</span>
+                <input className="w-96" type="password" value={rawPasswd} onChange={async e => {
+                    const raw = e.target.value
+                    setRawPasswd(raw)
+                    if(raw) {
+                        const salt = new Uint8Array(32)
+                        crypto.getRandomValues(salt)
+                        const hash = await generateHash(raw, salt)
+                        setPasswdHash(hash)
+                    } else {
+                        setPasswdHash('')
+                    }
+                }} />
+            </div>
+            <div className="z-10m py-2 w-200 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex shadow">
+                <span>Encrypted Password Hash (Click to copy)</span>
+                <input className="w-96" type="text" value={passwdHash} onClick={async (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+                    const hashedPasswd = (e.target as HTMLInputElement).value
+                    const x = e.clientX
+                    const y = e.clientY
+
+                    try {
+                        await navigator.clipboard.writeText(hashedPasswd)
+
+                        setModalVisible(true)
+                        setModalX(x)
+                        setModalY(y)
+                        setTimeout(() => setModalVisible(false), 5000)
+                    } catch (err) {
+                        console.error(err)
+                    }
+                }} readOnly />
+        <span style={{
+            visibility: modalVisible ? 'visible' : 'hidden',
+            position: 'absolute',
+            top: `${modalY + 20}px`,
+            left: `${modalX}px`
+        }} className="w-16 bg-blue-500 text-white text-center">Copied!</span>
+            </div>
+        </main>
+    )
+}
+
